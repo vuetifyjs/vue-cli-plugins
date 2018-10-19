d8a18ac776ed31f78dd2a7ad952cad90b4d84508
Zachary Russell <zach@protechig.com>
2018-10-19 14:40:55 -0400

    Bump year to 2018

--- generator/templates/default/src/App.vue
+++ generator/templates/default/src/App.vue
@@ -108,7 +108,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+      <span>&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
