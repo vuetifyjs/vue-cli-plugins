ce1d5d12ff10d42ebe2340a38bad6642347689e3
Kael <kaelwd@gmail.com>
2018-10-21 00:29:29 +1100

    fix: only require vuetify-loader if it's installed

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-
 module.exports = (api, opts) => {
   const hasVuetifyLoader = Boolean(
     api.service.pkg.devDependencies['vuetify-loader'] ||
@@ -7,6 +5,7 @@ module.exports = (api, opts) => {
   )
 
   if (hasVuetifyLoader) {
+    const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
     api.configureWebpack(webpackConfig => {
       webpackConfig.plugins.push(new VuetifyLoaderPlugin())
     })
