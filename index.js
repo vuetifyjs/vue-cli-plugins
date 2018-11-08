a6f664dbefa68db1ad7042b7454cb20875c5a399
Reto Aebersold <aeby@substyle.ch>
2018-11-08 13:49:10 +0100

    fix(webpack): remove webpack js include and use transpileDependencies instead Fixes #55

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = (api) => {
+module.exports = (api, opts) => {
   const hasVuetifyLoader = Boolean(
     api.service.pkg.devDependencies['vuetify-loader'] ||
     api.service.pkg.dependencies['vuetify-loader']
@@ -7,12 +7,13 @@ module.exports = (api) => {
   if (hasVuetifyLoader) {
     const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
+    // As the vuetify-loader automatically imports the necessary Vuetify components they are not found and
+    // transpiled by Babel. Add Vuetify explicitly as a module to be transpiled.
+    if (opts.transpileDependencies.indexOf('vuetify') === -1) {
+      opts.transpileDependencies.push('vuetify')
+    }
+
     api.chainWebpack(config => {
-      config.module
-        .rule('js')
-        .include
-          .add(api.resolve('node_modules/vuetify'))
-      
       config.plugin('VuetifyLoaderPlugin')
         .use(VuetifyLoaderPlugin)
     })
