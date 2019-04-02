3ea8a878b6d0c56dc3e08bc3ffa3caa78b97db07
Brian Helba <brian.helba@kitware.com>
2019-04-02 01:04:13 -0400

    Support asset references from v-img (#85)
    * Support asset references from v-img
    
    * Support asset references from lazy-src and v-parallax
    
    * Support asset references from v-carousel-item
    
    * Support asset references from v-app-bar and v-toolbar
    
    * Generalize a comment

--- index.js
+++ index.js
@@ -12,4 +12,23 @@ module.exports = (api) => {
         .use(VuetifyLoaderPlugin)
     })
   }
+
+  // Resolve asset references from components
+  api.chainWebpack(config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => ({
+        ...options,
+        transformAssetUrls: {
+          // v-app-bar extends v-toolbar
+          'v-app-bar': 'src',
+          // v-carousel-item extends v-img
+          'v-carousel-item': ['src', 'lazy-src'],
+          'v-img': ['src', 'lazy-src'],
+          'v-parallax': 'src',
+          'v-toolbar': 'src',
+        },
+      }))
+  })
 }
