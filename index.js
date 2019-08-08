fa4c35e888598ab74d74b0d4d218f3af044ebc47
John Leider <john.j.leider@gmail.com>
2019-08-08 19:33:44 -0400

    fix: add missing opts argument

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-module.exports = (api) => {
+module.exports = (api, opts) => {
   const hasVuetifyLoader = Boolean(
     api.service.pkg.devDependencies['vuetify-loader'] ||
     api.service.pkg.dependencies['vuetify-loader']
