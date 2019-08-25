b4916f84fff849d4b9de7adab9ae83bf2459b764
John Leider <john@vuetifyjs.com>
2019-08-24 23:59:30 -0400

    refactor: add goto functionality, clean up routes

--- packages/vue-cli-plugin-vuetify-essentials/generator/template/src/router.js
+++ packages/vue-cli-plugin-vuetify-essentials/generator/template/src/router.js
@@ -1,21 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import vuetify from './plugins/vuetify'
 
 Vue.use(Router)
+
+function route (path, name) {
+  return {
+    path,
+    name,
+    component: () => import(`@/views/${name.toLowerCase()}/Index`)
+  }
+}
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/views/home/Index')
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: () => import('@/views/about/Index')
+  scrollBehavior: (to, from, savedPosition) => {
+    let scrollTo = 0
+
+    if (to.hash) {
+      scrollTo = to.hash
+    } else if (savedPosition) {
+      scrollTo = savedPosition.y
     }
+
+    return vuetify.framework.goTo(scrollTo)
+  },
+  routes: [
+    route('/', 'Home'),
+    route('/about', 'About')
   ]
 })
