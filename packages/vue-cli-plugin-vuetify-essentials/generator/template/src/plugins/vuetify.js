e8b4204bfadbbc2107ec9fbdc84c2832b543b58f
John Leider <john.j.leider@gmail.com>
2019-08-26 12:25:52 -0500

    refactor(webfontloader): move to dedicated plugin file

--- packages/vue-cli-plugin-vuetify-essentials/generator/template/src/plugins/vuetify.js
+++ packages/vue-cli-plugin-vuetify-essentials/generator/template/src/plugins/vuetify.js
@@ -4,18 +4,8 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import colors from 'vuetify/lib/util/colors'
 import i18n from '@/i18n'
-import WebFontLoader from 'webfontloader'
 
 Vue.use(Vuetify)
-
-// async load fonts
-WebFontLoader.load({
-  google: {
-    families: [
-      'Roboto:100,300,400,500,700,900'
-    ]
-  }
-})
 
 export default new Vuetify({
   icons: {
