e09324534b529394b265186101d342e1df9e6db3
John Leider <john.j.leider@gmail.com>
2019-08-28 20:40:27 -0500

    fix: properly add vuetify eslint-config

--- packages/vue-cli-plugin-vuetify-essentials/generator/index.js
+++ packages/vue-cli-plugin-vuetify-essentials/generator/index.js
@@ -11,6 +11,7 @@ module.exports = api => {
       'vue-analytics': '*',
       'vue-meta': '*',
       'vuex-pathify': '*',
+      'eslint-config-vuetify': '*',
       'vuex-router-sync': '*'
     },
     eslintConfig: {
