cda6d0fd5a1d40e58d81c108a340be8cfa8fe1ba
John Leider <john.j.leider@gmail.com>
2019-10-14 10:44:12 -0500

    feat(BaseBoilerplate): create new component

---
+++ packages/vue-cli-plugin-vuetify-essentials/generator/template/src/components/base/Boilerplate.vue
@@ -0,0 +1,20 @@
+<script>
+  import { VSkeletonLoader } from 'vuetify/lib'
+
+  export default {
+    name: 'BaseBoilerplate',
+
+    extends: VSkeletonLoader,
+
+    props: {
+      boilerplate: {
+        type: Boolean,
+        default: true,
+      },
+      elevation: {
+        type: [Number, String],
+        default: 2,
+      },
+    },
+  }
+</script>
