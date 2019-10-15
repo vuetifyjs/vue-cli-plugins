5d71d749f5f405f41984eaab6310fce0a9a23238
John Leider <john.j.leider@gmail.com>
2019-10-15 16:40:57 -0500

    feat: init commit

---
+++ packages/vue-cli-plugin-vuetify-storybook/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const path = require('path')
+const kebabCase = require('lodash/kebabCase')
+
+module.exports = api => {
+  if (!api.hasPlugin('vuetify-cli')) return
+
+  const component = api.service.commands['make:component']
+
+  // If user removed or modified
+  // the script to a new name.
+  if (!component) return
+
+  const serveFn = component.fn
+
+  component.fn = async (...args) => {
+    const { name, rootDir, type } = await serveFn(...args)
+    let file
+
+    if (type.indexOf('../') === 0) {
+      const split = type.split('/')
+
+      split.shift()
+
+      file = `${split.join('/')}${name}`
+    } else {
+      file = `components/${type ? `${type}/` : ''}${name}/${name}`
+    }
+
+    const template =
+      fs.readFileSync(path.resolve(__dirname, './generator/template/template.stories.js'), 'utf-8')
+        .replace(/ComponentName/g, name)
+        .replace(/ComponentPath/g, file)
+        .replace(/component-template/g, kebabCase(name))
+
+    const dirName = kebabCase(name)
+
+    fs.writeFileSync(`${rootDir}/.storybook/stories/${dirName}.stories.js`, template, 'utf-8')
+  }
+}
