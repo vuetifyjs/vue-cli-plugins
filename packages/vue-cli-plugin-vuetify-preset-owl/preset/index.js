ae640bf04ffebfbc1e20e567981b718b5b192dca
John Leider <john.j.leider@gmail.com>
2019-12-19 15:18:53 -0500

    feat(owl): add new preset

---
+++ packages/vue-cli-plugin-vuetify-preset-owl/preset/index.js
@@ -0,0 +1,15 @@
+require('./overrides.sass')
+
+const preset = {
+  theme: {
+    themes: {
+      light: {
+        primary: '#FFDE03',
+        secondary: '#0336FF',
+        accent: '#FF0266',
+      },
+    },
+  },
+}
+
+module.exports = { preset }
