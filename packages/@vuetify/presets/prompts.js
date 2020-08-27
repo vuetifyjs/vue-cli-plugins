ee8615756d3c7a98ba6cb3b38599df3f00537a87
John Leider <john.j.leider@gmail.com>
2020-08-27 13:36:14 -0500

    style(prompts.js): clean-up code styling

--- packages/@vuetify/presets/prompts.js
+++ packages/@vuetify/presets/prompts.js
@@ -1,24 +1,24 @@
 module.exports = [
-    {
-        type: 'list',
-        name: 'preset',
-        message: 'Select from one of the following presets to scaffold your Vue project:',
-        choices: [
-            {
-                name: 'Base (A basic Vuetify project)',
-                value: 'base'
-            },
-            {
-                name: 'Essential (Coming soon)',
-                value: 'Essential',
-                disabled: true,
-            },
-            {
-                name: 'Recommended (Coming soon)',
-                value: 'recommended',
-                disabled: true,
-            },
-        ],
-        default: 'base',
-    }
+  {
+    type: 'list',
+    name: 'preset',
+    message: 'Select from one of the following presets to scaffold your Vue project:',
+    choices: [
+      {
+        name: 'Base (A basic Vuetify project)',
+        value: 'base'
+      },
+      {
+        name: 'Essential (Coming soon)',
+        value: 'essential',
+        disabled: true,
+      },
+      {
+        name: 'Recommended (Coming soon)',
+        value: 'recommended',
+        disabled: true,
+      },
+    ],
+    default: 'base',
+  }
 ]
