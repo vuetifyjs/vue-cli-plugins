0d19dbacc77eaca067932b749eef6cb60e94cdb6
John Leider <john.j.leider@gmail.com>
2020-08-27 19:18:30 -0500

    refactor(presets): update prompts file

--- packages/@vuetify/presets/prompts.js
+++ packages/@vuetify/presets/prompts.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     type: 'list',
     name: 'preset',
-    message: 'Select from one of the following presets to scaffold your Vue project:',
+    message: 'Select from one of the following presets to scaffold your Vuetify project:',
     choices: [
       {
         name: 'Base (A basic Vuetify project)',
@@ -13,33 +13,7 @@ module.exports = [
         value: 'essential',
         disabled: true,
       },
-      {
-        name: 'Recommended (Coming soon)',
-        value: 'recommended',
-        disabled: true,
-      },
     ],
     default: 'base',
   }
-  // {
-  //   type: 'confirm',
-  //   name: 'cstudy',
-  //   message: 'Install a Material Study preset?',
-  //   default: false
-  // },
-  // {
-  //   type: 'list',
-  //   name: 'study',
-  //   message: 'Select preset:',
-  //   when: answers => answers.cstudy,
-  //   choices: [
-  //     { name: 'Basil', value: 'basil' },
-  //     { name: 'Crane', value: 'crane' },
-  //     { name: 'Fortnightly', value: 'fortnightly' },
-  //     { name: 'Owl', value: 'owl' },
-  //     { name: 'Rally', value: 'rally' },
-  //     { name: 'Reply', value: 'reply' },
-  //     { name: 'Shrine', value: 'shrine' }
-  //   ]
-  // },
 ]
