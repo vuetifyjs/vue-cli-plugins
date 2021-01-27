de4e2b0728f623740d03a58a51ff09a4907885d0
Kael <kaelwd@gmail.com>
2021-01-27 18:48:45 +1100

    fix: install sass-loader@10 by default

--- packages/vue-cli-plugin-vuetify/generator/tools/alaCarte.js
+++ packages/vue-cli-plugin-vuetify/generator/tools/alaCarte.js
@@ -3,7 +3,7 @@ function addDependencies (api) {
     devDependencies: {
       'vuetify-loader': '^1.7.0',
       sass: '^1.32.0',
-      'sass-loader': '^8.0.0',
+      'sass-loader': '^10.0.0',
     },
   })
 }
