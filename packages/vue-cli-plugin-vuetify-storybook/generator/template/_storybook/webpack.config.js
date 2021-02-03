d111d667286ca6525628f8807248c73725a51938
Morgan Benton <morgan.benton@gmail.com>
2021-02-03 15:01:15 -0500

    feat(webpack.config): add src alias (#174)
    Added `@` to storybook `webpack.config.js` as an alias for `./src` to match default Vue webpack
    config.

--- packages/vue-cli-plugin-vuetify-storybook/generator/template/_storybook/webpack.config.js
+++ packages/vue-cli-plugin-vuetify-storybook/generator/template/_storybook/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = async ({ config }) => {
   config.resolve.alias['~storybook'] = path.resolve(__dirname)
+  config.resolve.alias['@'] = path.resolve(__dirname, '..', 'src')
 
   config.module.rules.push({
     resourceQuery: /blockType=story/,
