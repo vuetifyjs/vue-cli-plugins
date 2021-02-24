01e2e5f54853dbe13f1a2744cfa5ca60f6f9a53e
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-02-24 14:46:33 -0800

    chore(v3): start building template.

---
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/index.js
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import vuetify from "./vuetify";
+import App from "./App";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+
+const app = createApp(App);
+
+app.use(vuetify);
+app.component("FontAwesomeIcon", FontAwesomeIcon);
+
+app.mount("#app");
---
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/src/iconsets/mdi";
+import { fa } from "vuetify/src/iconsets/fa-svg";
+
+export default createVuetify({
+  // lang: {
+  //   locales,
+  // },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+      fa,
+    },
+  },
+});
