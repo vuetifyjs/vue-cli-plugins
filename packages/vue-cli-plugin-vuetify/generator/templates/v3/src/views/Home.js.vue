532fdab0dae930e7108ceaed52f73400f856be3d
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-02-25 13:37:56 -0800

    chore(templates): add base v3 templates for JS.

---
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/views/Home.js.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HelloWorld msg="Welcome to Your Vuetify.js App" />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld.vue'
+
+  export default {
+    name: 'Home',
+    
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
