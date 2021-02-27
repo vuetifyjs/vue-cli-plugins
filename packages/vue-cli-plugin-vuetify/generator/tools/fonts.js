35afbe285e83b02c292b70bb670a6d02bda9ff1a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-02-27 12:25:31 -0800

    style(fonts): remove double quotes on font strings.

--- packages/vue-cli-plugin-vuetify/generator/tools/fonts.js
+++ packages/vue-cli-plugin-vuetify/generator/tools/fonts.js
@@ -2,41 +2,41 @@ const helpers = require('./helpers')
 const fonts = {
   mdi: {
     package: {
-      "@mdi/font": "5.9.55",
+      '@mdi/font': '5.9.55',
     },
-    import: "@mdi/font/css/materialdesignicons.css",
+    import: '@mdi/font/css/materialdesignicons.css',
     link:
       '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">',
   },
   md: {
     package: {
-      "material-design-icons-iconfont": "latest",
+      'material-design-icons-iconfont': 'latest',
     },
-    import: "material-design-icons-iconfont/dist/material-design-icons.css",
+    import: 'material-design-icons-iconfont/dist/material-design-icons.css',
     link:
       '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons">',
   },
   fa: {
     package: {
-      "@fortawesome/fontawesome-free": "latest",
+      '@fortawesome/fontawesome-free': 'latest',
     },
-    import: "@fortawesome/fontawesome-free/css/all.css",
+    import: '@fortawesome/fontawesome-free/css/all.css',
     link:
       '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">',
   },
   fa4: {
     package: {
-      "font-awesome": "latest",
+      'font-awesome': 'latest',
     },
-    import: "font-awesome/css/font-awesome.css",
+    import: 'font-awesome/css/font-awesome.css',
     link:
       '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">',
   },
   roboto: {
     package: {
-      "roboto-fontface": "*",
+      'roboto-fontface': '*',
     },
-    import: "roboto-fontface/css/roboto/roboto-fontface.css",
+    import: 'roboto-fontface/css/roboto/roboto-fontface.css',
     link:
       '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">',
   },
