ac2c30ce917b9c2e7933ee23a8141f1bddaf76f1
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-03-01 12:32:31 -0800

    fix(fonts): @mdi/font version (#240)

--- packages/vue-cli-plugin-vuetify/generator/tools/fonts.js
+++ packages/vue-cli-plugin-vuetify/generator/tools/fonts.js
@@ -2,38 +2,43 @@ const helpers = require('./helpers')
 const fonts = {
   mdi: {
     package: {
-      '@mdi/font': 'latest',
+      '@mdi/font': '5.9.55',
     },
     import: '@mdi/font/css/materialdesignicons.css',
-    link: '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">',
+    link:
+      '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">',
   },
   md: {
     package: {
       'material-design-icons-iconfont': 'latest',
     },
     import: 'material-design-icons-iconfont/dist/material-design-icons.css',
-    link: '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons">',
+    link:
+      '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons">',
   },
   fa: {
     package: {
       '@fortawesome/fontawesome-free': 'latest',
     },
     import: '@fortawesome/fontawesome-free/css/all.css',
-    link: '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">',
+    link:
+      '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">',
   },
   fa4: {
     package: {
       'font-awesome': 'latest',
     },
     import: 'font-awesome/css/font-awesome.css',
-    link: '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">',
+    link:
+      '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">',
   },
   roboto: {
     package: {
       'roboto-fontface': '*',
     },
     import: 'roboto-fontface/css/roboto/roboto-fontface.css',
-    link: '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">',
+    link:
+      '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">',
   },
 }
 
