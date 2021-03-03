89d3ba31fc1f84c6d8f464c6f3dbf34ac2bb82c9
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-03-02 19:26:30 -0800

    chore(v3-preset): update plugin and js entry with valid config.

--- packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
@@ -1,18 +1,4 @@
 import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
-import { aliases, mdi } from "vuetify/src/iconsets/mdi";
-import { fa } from "vuetify/src/iconsets/fa-svg";
 
-export default createVuetify({
-  // lang: {
-  //   locales,
-  // },
-  icons: {
-    defaultSet: "mdi",
-    aliases,
-    sets: {
-      mdi,
-      fa,
-    },
-  },
-});
+export default createVuetify();
