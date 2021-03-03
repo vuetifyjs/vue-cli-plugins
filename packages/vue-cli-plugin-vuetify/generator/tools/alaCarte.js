cc969bdda4291fccbf04d103bb5374b5db570956
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-03-03 00:08:20 -0800

    fix: Update template imports and don't install vuetify-loader (#243)

--- packages/vue-cli-plugin-vuetify/generator/tools/alaCarte.js
+++ packages/vue-cli-plugin-vuetify/generator/tools/alaCarte.js
@@ -1,11 +1,14 @@
-function addDependencies (api) {
-  api.extendPackage({
+function addDependencies (api, useV3) {
+  let deps = {
     devDependencies: {
-      'vuetify-loader': '^1.7.0',
       sass: '^1.32.0',
       'sass-loader': '^10.0.0',
     },
-  })
+  }
+
+  if (!useV3) deps['vuetify-loader'] = '^1.7.0'
+
+  api.extendPackage(deps);
 }
 
 function addVueConfigTranspileDependency (api) {
