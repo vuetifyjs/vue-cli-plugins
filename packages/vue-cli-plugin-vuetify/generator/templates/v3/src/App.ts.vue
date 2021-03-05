49206585e5204f26224726561d1b8e25f67fb6f0
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-03-04 21:44:54 -0800

    feat(v3-preset): add v3 template files.

---
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/App.ts.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-main>
+      <%_ if (router) { _%>
+      <router-view/>
+      <%_ } else { _%>
+      <HelloWorld/>
+      <%_ } _%>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+<%_ if (!router) { _%>
+import HelloWorld from './components/HelloWorld.vue';
+<%_ } _%>
+
+export default defineComponent({
+  name: 'App',
+
+  <%_ if (!router) { _%>
+  components: {
+    HelloWorld,
+  },
+
+  <%_ } _%>
+  data () {
+    return {
+      //
+    }
+  },
+});
+</script>
---
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
+import App from './App.vue'
+<%_ if (router) { _%>
+import router from './router'
+<%_ } _%>
+
+const app = createApp(App);
+
+<%_ if (router) { _%>
+app.use(router)
+<%_ } _%>
+
+app.use(vuetify)
+
+app.mount('#app')
