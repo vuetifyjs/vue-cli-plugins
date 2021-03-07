6255f7beb9bb53b46bc7e25f9bfffe8e3d912bad
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-03-06 20:54:22 -0800

    style(vite-config): remove spacing
    Co-authored-by: John Leider <9064066+johnleider@users.noreply.github.com>

--- packages/vue-cli-plugin-vuetify/generator/templates/v3/src/vite.config.js
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/vite.config.js
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'process.env': {},
-  },
+  define: { 'process.env': {} },
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
