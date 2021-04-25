f211ce51433fbfe20cefd8097f4750df62b35742
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-04-25 14:58:58 -0700

    refactor(storybook): alphabetize addons.
    Co-authored-by: John Leider <9064066+johnleider@users.noreply.github.com>

--- packages/@vuetify/storybook/generator/template/_storybook/main.js
+++ packages/@vuetify/storybook/generator/template/_storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
   stories: ['./stories/**/*.stories.js'],
   addons: [
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-viewport/register',
-    '@storybook/addon-a11y/register',
-    '@storybook/addon-docs/register',
-    './addon-show-vue-markup/register',
+  '@storybook/addon-a11y/register',
+  '@storybook/addon-actions/register',
+  '@storybook/addon-docs/register',
+  '@storybook/addon-viewport/register',
+  '@storybook/addon-knobs/register',
+  './addon-show-vue-markup/register',
   ],
 }
