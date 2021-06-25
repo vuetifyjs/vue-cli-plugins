a844235a911bafc23bb1210eb0cd221bd15c4767
John Leider <john.j.leider@gmail.com>
2021-06-25 16:41:22 -0500

    fix(vite): add support for css variables

--- packages/vue-cli-plugin-vuetify/generator/templates/v3/src/vite.config.js
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'path'
+const srcPath = path.resolve(__dirname, 'src', 'styles', 'variables.scss')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   define: { 'process.env': {} },
+  css: {
+    preprocessorOptions: {
+      sass: { additionalData: `@import ${srcPath}\n` },
+      scss: { additionalData: `@import ${srcPath};\n` },
+    },
+  },
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
