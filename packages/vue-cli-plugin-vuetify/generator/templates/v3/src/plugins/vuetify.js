81281eca6aafe5bdf00a0cf588f7e90c0436f82c
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-09-19 18:17:51 -0700

    chore(alpha.11): change path for plugin imports.

--- packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
@@ -1,8 +1,8 @@
 import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/lib/styles/main.sass'
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components'
-import * as directives from 'vuetify/lib/directives'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 export default createVuetify({
   components,
--- packages/vue-cli-plugin-vuetify/generator/tools/alaCarte.js
+++ packages/vue-cli-plugin-vuetify/generator/tools/alaCarte.js
@@ -1,12 +1,13 @@
 function addDependencies (api, useV3) {
   const deps = {
     devDependencies: {
-      sass: '^1.38.0',
-      'sass-loader': '^10.0.0',
+      sass: useV3 ? "^1.38.0" : "~1.32.0",
+      "sass-loader": "^10.0.0",
     },
-  }
+  };
 
-  if (!useV3) deps.devDependencies['vuetify-loader'] = '^1.7.0'
+  if (useV3) deps.devDependencies['@vue/cli-service'] = '~5.0.0-beta.3'
+  else deps.devDependencies['vuetify-loader'] = '^1.7.0'
 
   api.extendPackage(deps)
 }
