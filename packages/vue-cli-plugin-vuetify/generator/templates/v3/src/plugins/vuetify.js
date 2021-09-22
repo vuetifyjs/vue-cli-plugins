dcd84fcd66a105d405ae6ecfec727793ebe721b0
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-09-21 21:57:15 -0700

    refactor(v3-template): remove unnecessary imports.

--- packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
@@ -1,10 +1,5 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-export default createVuetify({
-  components,
-  directives,
-})
+export default createVuetify()
--- packages/vue-cli-plugin-vuetify/generator/templates/v3/src/vite.config.js
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vuetify from "@vuetify/vite-plugin";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from '@vuetify/vite-plugin';
 
-import path from "path";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,13 +10,12 @@ export default defineConfig({
     vue(),
     vuetify({
       autoimport: true,
-      styles: "expose",
     }),
   ],
-  define: { "process.env": {} },
+  define: { 'process.env': {} },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
