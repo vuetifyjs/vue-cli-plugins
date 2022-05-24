0e469776b7e5b318a52bf241e72cdba925ca3ccd
Elijah Kotyluk <elijah@elijahkotyluk.com>
2022-05-24 07:55:19 -0700

    refactor(generator): only add vuetify-loader when not vite.

--- packages/vue-cli-plugin-vuetify/generator/index.js
+++ packages/vue-cli-plugin-vuetify/generator/index.js
@@ -24,13 +24,14 @@ module.exports = (api, opts) => {
 
   // Add dependencies
   vuetify.addDependencies(api, opts)
-  if (opts.useAlaCarte) alaCarte.addDependencies(api, opts.useV3)
+  if (opts.useAlaCarte && !opts.useVite) alaCarte.addDependencies(api, opts.useV3)
   else if (opts.usePolyfill) polyfill.addDependencies(api)
 
   // Vite
   if (opts.useVite) {
     vite.addDependencies(api)
     vite.renderFiles(api, opts)
+    vite.updateViteConfig(api, opts)
   }
 
   if (opts.installFonts) fonts.addDependencies(api, opts.iconFont)
@@ -51,14 +52,18 @@ module.exports = (api, opts) => {
     if (!opts.installFonts) fonts.addLinks(api, opts.iconFont)
     vuetify.setHtmlLang(api, opts.locale)
 
-    if (fileExists(api, 'src/public/index.html')) {
-      fs.unlinkSync(api.resolve('src/public/index.html'))
+    if (fileExists(api, './src/public/index.html')) {
+      fs.unlinkSync(api.resolve('./src/public/index.html'))
     }
 
     const configFile = api.resolve('./vue.config.js')
 
     if (fileExists(api, configFile)) {
-      vuetify.addVuetifyLoaderDocsLink(configFile)
+      if (opts.useVite) {
+        fs.unlinkSync(configFile)
+      } else {
+        vuetify.addVuetifyLoaderDocsLink(configFile)
+      }
     }
 
     api.exitLog('Discord community: https://community.vuetifyjs.com')
