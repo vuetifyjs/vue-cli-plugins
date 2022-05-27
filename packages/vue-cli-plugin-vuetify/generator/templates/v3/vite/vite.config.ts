09176049a665f4fb5f3c8f660033e363ee88269c
Elijah Kotyluk <elijah@elijahkotyluk.com>
2022-05-27 10:36:06 -0700

    fix(template): update vite plugin vuetify import.

--- packages/vue-cli-plugin-vuetify/generator/templates/v3/vite/vite.config.ts
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/vite/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
+import vuetify from 'vite-plugin-vuetify'
 
 const path = require('path')
 
