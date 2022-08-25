7710c94af8a0bb45eed705bb4d2e254a8a664b9d
Fabricius Seifert <fabriciusseifert@gmail.com>
2022-08-25 15:29:53 +0200

    enabling components to be rendered when starting Template the first time
    I noticed that when I first start the template for Vite, no components are displayed. These lines have fixed the problem. Hope this helps.

--- packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
+++ packages/vue-cli-plugin-vuetify/generator/templates/v3/src/plugins/vuetify.js
@@ -1,10 +1,16 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
 export default createVuetify(
+  {
+    components,
+    directives,
+  }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 )
